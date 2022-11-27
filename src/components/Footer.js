2c4e5c4f2512ad49657cbbe9b954d0a7a9864a87
Kel-VinTech <mondaykelvintech@gmail.com>
2022-11-27 10:33:40 -0500

    updated project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import React from "react";
 const Footer =() => {
     return(
         <div className="footer row">
-            <a href="https://github.com/">
-                <i class="fa-brands fa-github"></i>
+            <a href="https://twitter.com/">
+            <i class="fa-brands fa-twitter"></i>
                 </a>
-                <a href="https://github.com/">
-                <i class="fa-brands fa-github"></i>
+                <a href="https://facebook.com/">
+                <i class="fa-brands fa-facebook"></i>
                 </a>
-                <a href="https://github.com/">
-                <i class="fa-brands fa-github"></i>
+                <a href="https://instagram.com/">
+                <i class="fa-brands fa-instagram"></i>
                 </a>
                 <a href="https://github.com/">
                 <i class="fa-brands fa-github"></i>
--- src/components/HeroContent.js
+++ src/components/HeroContent.js
@@ -3,15 +3,15 @@ import React  from "react";
 const HeroContent = ( ) => {
     return (
         <div className="hero row">
-            <h1>Laura Smith</h1>
+            <h1>Paul Mccall</h1>
             <h3>Frontend Developer</h3>
-            <a className="hero_web" href="www.google.com">LauraSmith.website</a>
+            <a className="hero_web" href="www.google.com">Paulmccall.website</a>
             <div className="hero__btn">
-                <a href="https://mail.google.com/mail">
+                <a className="web_mail" href="https://mail.google.com/mail">
                 <i class="fa-solid fa-envelope"></i>Email
                 </a>
-                <a href="https://github.com/">
-                <i class="fa-brands fa-github"></i>Github
+                <a className="web_link" href="https://linkedin.com/">
+                <i class="fa-brands fa-linkedin"></i>Linkedin
                 </a>
             </div>
         </div>
